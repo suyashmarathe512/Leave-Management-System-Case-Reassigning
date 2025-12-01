093b08d8b606c2ced6c038f5d67ca9bb60a58695
Suyash <suyashmarathe1092003@gmail.com>
2025-12-01 15:37:13 +0530

    second commit

--- Leave Management System/force-app/main/default/lwc/leaveBalances/leaveBalances.js
+++ Leave Management System/force-app/main/default/lwc/leaveBalances/leaveBalances.js
@@ -1,11 +1,46 @@
 import { LightningElement, wire } from "lwc";
 import getLeaveBalance from "@salesforce/apex/LeaveApplicationController.getLeaveBalance";
+import getDefaultLeaveBalanceSetting from "@salesforce/apex/LeaveApplicationController.getDefaultLeaveBalanceSetting";
 
 export default class LeaveBalances extends LightningElement {
     balance = {};
+    setting = {};
+    hasError = false;
 
     @wire(getLeaveBalance)
-    wiredLeaveBalance(result) {
-        this.balance = result.data ? result.data : {};
+    wiredLeaveBalance({ data, error }) {
+        if (data) {
+            this.balance = data;
+            this.hasError = false;
+        } else if (error) {
+            this.balance = {};
+            this.hasError = true;
+            // optional: surface toast in parent if needed
+        }
+    }
+
+    @wire(getDefaultLeaveBalanceSetting)
+    wiredSetting({ data, error }) {
+        if (data) {
+            this.setting = data;
+        } else {
+            this.setting = {};
+        }
+    }
+
+    get earnedCasualHours() {
+        return this.balance?.annualLeaveHours ?? 0;
+    }
+
+    get sickHours() {
+        return this.balance?.sickLeaveHours ?? 0;
+    }
+
+    get settingEarnedCasual() {
+        return this.setting?.earnedCasualLeave ?? 0;
+    }
+
+    get settingSick() {
+        return this.setting?.sickLeave ?? 0;
     }
 }
